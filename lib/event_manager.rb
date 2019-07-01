da6d430af5408bdd590f59a4c79b4e948d47c2bb
clair13 <theclairvoyant1313@yahoo.gr>
2019-07-01 20:01:58 +0300

    Display zipcodes

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -5,5 +5,6 @@ puts "EventManager Initialized!"
 content = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 content.each do | row |
   name = row[:first_name]
-  puts name
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
 end
