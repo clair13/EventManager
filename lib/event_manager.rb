f466d12c3bd9a24d9950b3f61f7fdcfa737b87ef
clair13 <theclairvoyant1313@yahoo.gr>
2019-07-01 19:57:32 +0300

    Implement CSV Library

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,9 @@
+
+require "csv"
 puts "EventManager Initialized!"
 
-lines = File.readlines "event_attendees.csv"
-lines.each_with_index do | line,index |
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
+content = CSV.open "event_attendees.csv", headers: true
+content.each do | row |
+  name = row[2]
   puts name
 end
