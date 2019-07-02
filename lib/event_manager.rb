8b9844ece8168b5b317b06e70654770f8095b26a
clair13 <theclairvoyant1313@yahoo.gr>
2019-07-02 21:05:46 +0300

    Implement personal letter

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -26,14 +26,19 @@ end
 
 puts "EventManager initialized."
 
+template_letter = File.read "form_letter.html"
+
 contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
 
 contents.each do |row|
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
-  
+
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  personal_letter = template_letter.gsub('FIRST_NAME',name)
+  personal_letter = template_letter.gsub!('LEGISLATORS',legislators)
+
+  puts personal_letter
 end
